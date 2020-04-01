5eec9e94cc9137398e13c4021fedc7a1b6e4105a
Dominc Carolan <dominiccarolan@gmail.com>
2020-04-01 22:58:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,13 +14,13 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-roll_call_dwarves(dwarves)
+
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
-summon_captain_planet(planeteer_calls)
+
 
 def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
@@ -28,7 +28,7 @@ end
 
 long_planeteer_calls(planeteer_calls)
 
-potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
+
 
 def find_the_cheese(potentially_cheesy_items)
   cheeses = %w[gouda cheddar camembert]
@@ -37,5 +37,3 @@ def find_the_cheese(potentially_cheesy_items)
     cheeses.include?(maybe_cheese)
   end
 end
-
-find_the_cheese(potentially_cheesy_items)
